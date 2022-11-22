5ffcdd0b251537fc260c2703de2f57054333211d
Edwin Rajkumar <info@lifethreadsalbums.com>
2022-11-21 21:42:28 -0500

    adding rename description updates scripts with rollback

---
+++ pace-sql/Lta-price-update-2022.sql
@@ -0,0 +1,45 @@
+UPDATE T_PRODUCT_OPTION_VALUE
+SET VERSION=0, DESCRIPTION='Standard Prints', NAME='Standard Prints', CODE='silver_halide', PARAMS='{
+    "title": "Standard Prints",
+    "description": "Our standard prints are made on our new eco friendly dry lab system.  We make exclusive use of Fuji papers and premium inks to produce prints that have excellent & accurate colour, outstanding archival quality and, most importantly, superb smile factor.",
+    "slideshow": [
+        {
+            "thumbUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SILVER_HALIDE_01-thumb.jpg",
+            "imageUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SILVER_HALIDE_01.jpg"
+        },
+        {
+            "thumbUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SILVER_HALIDE_02-thumb.jpg",
+            "imageUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SILVER_HALIDE_02.jpg"
+        }
+    ]
+}
+', T_RESOURCE_ID=NULL, T_PRODUCT_OPTION_TYPE=63, CREATED=NULL, MODIFIED=NULL
+WHERE ID=193;
+
+
+UPDATE T_PRODUCT_OPTION_VALUE
+SET VERSION=0, DESCRIPTION='Paper Type', NAME='Lustre', CODE='lustre', PARAMS='{
+    "title": "Lustre",
+    "description": "We print on Fuji Lustre papers.  Think of this paper as the \"Goldilocks\" option - just right.  Great colour, good contrast, good blacks, great whites, you get the idea.  It just works!",
+    "slideshow": [
+        {
+            "thumbUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SH_LUSTRE_01-thumb.jpg",
+            "imageUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SH_LUSTRE_01.jpg"
+        },
+        {
+            "thumbUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SH_lustre_02a-thumb.jpg",
+            "imageUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SH_lustre_02a.jpg"
+        },
+        {
+            "thumbUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SH_lustre_03-thumb.jpg",
+            "imageUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SH_lustre_03.jpg"
+        },
+        {
+            "thumbUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SH_lustre_close-thumb.jpg",
+            "imageUrl": "https://lifethreadsalbums.s3.amazonaws.com/builder/spo/SH_lustre_close.jpg"
+        }
+    ]
+}
+', T_RESOURCE_ID=400, T_PRODUCT_OPTION_TYPE=23, CREATED=NULL, MODIFIED=NULL
+WHERE ID=59;
+
